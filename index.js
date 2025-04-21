f963411b80daa83c593cc87c46ba65136427901c
Wandile Langa <langamitch69@gmail.com>
2025-04-21 10:33:12 +0200

    Updated

--- index.js
+++ index.js
@@ -6,4 +6,12 @@ function updateClock() {
   
   setInterval(updateClock, 1000);
   updateClock(); // Initial call
-  
+
+
+  function toggleSidebar() {
+    const sidebar = document.getElementById('sidebar');
+    const overlay = document.getElementById('overlay');
+    sidebar.classList.toggle('active');
+    overlay.classList.toggle('active');
+  }
+
